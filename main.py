2b488ba384fb70a3c6dd12172147c2283dc65d8f
alex <alex@x1carbon.home>
2025-03-02 21:15:17 +0100

    cleaning flash anzan

--- main.py
+++ main.py
@@ -1,11 +1,20 @@
 import tkinter as tk
+import sys
 from src.algo.hard_gen_class import HardNumberGenerator
 from flash_anzan_app import FlashAnzanApp
 
 def main():
+    delay = None
+
+    for arg in sys.argv[1:]:
+        if arg.startswith("-delay"):
+            delay=int(arg.split("=")[1])
+
+    if delay is None:
+        return
     root = tk.Tk()
-    hard_gen = HardNumberGenerator(num_count=5)
-    app = FlashAnzanApp(root, num_count=2, display_time=500, number_generator=hard_gen)
+    hard_gen = HardNumberGenerator(num_count=10)
+    app = FlashAnzanApp(root, num_count=2, display_time=delay, number_generator=hard_gen)
     root.mainloop()
 
 if __name__ == '__main__':
